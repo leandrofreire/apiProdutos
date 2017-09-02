d1b677dc23d72190c3841243422a2d3502f20132
leandrofreire <rafaelleandrofreire@gmail.com>
2017-09-02 16:02:35 -0300

    Config database

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-107-21-109-15.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dffb12bal7ocn0'),
+            'username' => env('DB_USERNAME', 'vesndhesymzjdc'),
+            'password' => env('DB_PASSWORD', '5974adab4ced8eabed6c3af3bb85cf1377bb2e1d97e33eee2c220382d57adced
+'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
