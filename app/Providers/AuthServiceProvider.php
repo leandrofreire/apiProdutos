9ad50aed45282d11f4321b24b4816be41144bf40
leandrofreire <rafaelleandrofreire@gmail.com>
2017-09-10 21:11:26 -0300

    Definindo tempo de expiração do token de acesso na API

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Carbon\Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,6 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(10));
     }
 }
